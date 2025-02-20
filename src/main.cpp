51eba7934eaf1e0e1cd45b482b0c19d86e939ef5
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2025-02-20 20:36:40 +0000

    Simple in class vector example

--- src/main.cpp
+++ src/main.cpp
@@ -4,19 +4,56 @@
 // Date:        
 // Class:       
 // Assignment:  
-// Purpose:     Simple driver to execute SampleClass class
+// Purpose:     
 // Hours:       
 //******************************************************************************
 
 #include <iostream>
-#include "../include/SampleClass.h"
+#include <vector>
+#include <random>
+
+void fill (std::vector<int>& rcVec, size_t size, unsigned int upper);
+void print (const std::vector<int>& rcVec);
 
 int main () {
-  SampleClass cSampleClass;
+  // std::vector<int> cVec (5);
+  std::vector<int> cVec;
 
-  std::cout << "Factorial of 1: " << cSampleClass.factorial (0) << std::endl;
+  srand (0);
 
-  std::cout << "Process Completed" << std::endl;
+  std::cout << std::endl;
+  std::cout << "Uninitialized Vector: ";
+  print (cVec);
+  std::cout << std::endl << std::endl;
+  std::cout << "cVec Capacity: " << cVec.capacity () << std::endl;
+  std::cout << "cVec Size: " << cVec.size () << std::endl << std::endl;
+
+  fill (cVec, 5, 10);
+  std::cout << "Filled Vector: ";
+  print (cVec);
+  std::cout << std::endl << std::endl;
+  std::cout << "cVec Capacity: " << cVec.capacity () << std::endl;
+  std::cout << "cVec Size: " << cVec.size () << std::endl;
+
+  cVec.clear ();
+  if (cVec.empty ()) {
+    std::cout << "Vector is empty!";
+  }
+  std::cout << std::endl << std::endl;
+  std::cout << "cVec Capacity: " << cVec.capacity () << std::endl;
+  std::cout << "cVec Size: " << cVec.size () << std::endl << std::endl;
 
   return EXIT_SUCCESS;
+}
+
+void print (const std::vector<int>& rcVec) {
+  for (int i = 0; i < rcVec.size (); i++) {
+    std::cout << rcVec[i] << " ";
+  }
+}
+
+void fill (std::vector<int>& rcVec, size_t size, unsigned int upper) {
+  for (size_t i = 0; i < size; i++) {
+    rcVec.push_back (rand () % upper);
+  }
 }
